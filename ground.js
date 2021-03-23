bd89d82e38403f8b8b7dfd2d5858d1bee4444c74
shivi09 <jainshivu09@gmail.com>
2021-03-23 22:04:45 +0530

    Update ground.js

--- ground.js
+++ ground.js
@@ -1,28 +1,18 @@
 
 class Ground{
-    constructor(x,y,width,height,fillColor){
+    constructor(x,y,width,height){
         var options={
-            restitution:1,
-            friction:1,
-            density:1,
             isStatic:true
         }
         this.body=Bodies.rectangle(x,y,width,height,options);
         World.add(world,this.body);
         this.width=width;
         this.height=height;
-        this.color=fillColor;
     }
     display(){
         var pos = this.body.position;
-        fill(this.color);
-        push();
-        translate(pos.x,pos.y);
-        angleMode(RADIANS);
-        rotate(this.body.angle);
+        fill("brown");
         rectMode(CENTER);
-        rect(0,0,this.width,this.height);
-        pop();
-
+        rect(pos.x,pos.y,this.width,this.height);
     }
 }
